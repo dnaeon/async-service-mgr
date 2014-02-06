bb6dd726bf4d8d10d1a72774b838e4a15fbbc384
Marin Atanasov Nikolov <dnaeon@gmail.com>
2014-02-06 19:00:47 +0200

    Initial commit of setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(name='service-mgr',
+      version='0.1.0',
+      description='Asynchronous Service Manager for UNIX/Linux systems',
+      author='Marin Atanasov Nikolov',
+      author_email='dnaeon@gmail.com',
+      license='BSD',
+      packages=['service'],
+      package_dir={'': 'src'},
+      scripts=[
+      ],
+      install_requires=[
+        'pyzmq >= 13.1.0',
+        'docopt >= 0.6.1',
+      ]
+)
+
